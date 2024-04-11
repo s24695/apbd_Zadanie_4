24308f052b980968972c39b1fc34e62a4a955cc3
s24695 <s24695@pjwstk.edu.pl>
2024-04-11 21:12:53 +0200

    nie wiem co robie

--- obj/Debug/net8.0/APBDzad4.AssemblyInfo.cs
+++ obj/Debug/net8.0/APBDzad4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APBDzad4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78231751acc715845e6ea04dbdebd8f5a1526175")]
 [assembly: System.Reflection.AssemblyProductAttribute("APBDzad4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APBDzad4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
